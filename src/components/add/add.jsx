1ee4ebc5dec4a9f40e6b42d3aacd51ba25f51831
mayanksh99 <mayankshakya992@gmail.com>
2020-02-10 01:28:02 +0530

    --update:add form complete

--- src/components/add/add.jsx
+++ src/components/add/add.jsx
@@ -21,9 +21,18 @@ const fields = [
 
 export default function AddForm(props) {
   let data = useContext(AuthContext);
-  const [x, handleX] = useInputState("");
-  const [y, handleY] = useInputState("");
-  const [type, handleType] = useInputState("");
+  const [name, handleName] = useInputState("");
+  const [location, handleLocation] = useInputState("");
+  const [size, handleSize] = useInputState("");
+  const [web, handleWeb] = useInputState("");
+  const [fb, handleFB] = useInputState("");
+  const [insta, handleInsta] = useInputState("");
+  const [twitter, handleTwitter] = useInputState("");
+  const [medium, handleMedium] = useInputState("");
+  const [git, handleGit] = useInputState("");
+  const [yt, handleYT] = useInputState("");
+  const [In, handleIN] = useInputState("");
+
   const [option, setOption] = useState({ selectedOption: [] });
 
   const defaultOptions = [
@@ -45,160 +54,121 @@ export default function AddForm(props) {
   //   console.log(domain);
   // }
 
-  useEffect(() => {
-    console.log("chl");
-  }, [x, y, type]);
-  const handleSubmit = async e => {
-    e.preventDefault();
-    let body = { latitude: x, longitude: y, type: type };
-    try {
-      const token = data.token;
-      await axios.post(add, body, {
-        headers: {
-          "x-auth-token": token
-        }
-      });
-      props.history.push("/");
-      console.log("API REQUEST");
-    } catch (error) {
-      console.log(error);
-      console.log(error.response);
-    }
-  };
+  // useEffect(() => {
+  //   console.log("chl");
+  // }, [x, y, type]);
 
-  console.log(option);
+  // const handleSubmit = async e => {
+  //   e.preventDefault();
+  //   let body = { latitude: x, longitude: y, type: type };
+  //   try {
+  //     const token = data.token;
+  //     await axios.post(add, body, {
+  //       headers: {
+  //         "x-auth-token": token
+  //       }
+  //     });
+  //     props.history.push("/");
+  //     console.log("API REQUEST");
+  //   } catch (error) {
+  //     console.log(error);
+  //     console.log(error.response);
+  //   }
+  // };
+
   return (
-    // <div className="container">
-    //   <div className={`col-lg-8 mx-auto ${styles.crdEdit}`}>
-    //     <div className="pt-5">
-    //       <p className={`heading ${styles.head}`}>Enter Data</p>
-    //     </div>
-    //     <div className="card p-4">
-    //       <form onSubmit={handleSubmit}>
-    //         <label className="label">Co-Ordinates : </label>
-
-    //         <div className="form-group row mt-2">
-    //           <label className="col-sm-2 col-form-label">X-Axis :</label>
-    //           <div className="col-sm-3">
-    //             <input
-    //               type="number"
-    //               name="x"
-    //               className="form-control"
-    //               placeholder="X"
-    //               value={x}
-    //               onChange={handleX}
-    //             />
-    //           </div>
-    //           <label className="col-sm-2 col-form-label">Y-Axis :</label>
-    //           <div className="col-sm-3">
-    //             <input
-    //               type="number"
-    //               name="y"
-    //               className="form-control"
-    //               placeholder="Y"
-    //               value={y}
-    //               onChange={handleY}
-    //             />
-    //           </div>
-    //         </div>
-    //         <div className="row mt-4">
-    //           <label className="col-sm-2 label">Type : </label>
-    //           <div className="col-sm-6 form-group ">
-    //             <select
-    //               name="type"
-    //               className="form-control"
-    //               id="exampleFormControlSelect1"
-    //               value={type}
-    //               onChange={handleType}
-    //             >
-    //               <option defaultChecked>Select</option>
-    //               <option value="crime">Crime</option>
-    //               <option value="accident">Accident</option>
-    //             </select>
-    //           </div>
-    //         </div>
-    //         <div className="text-right">
-    //           <button type="submit" className="btn btn-primary">
-    //             Submit
-    //           </button>
-    //         </div>
-    //       </form>
-    //     </div>
-    //   </div>
-    // </div>
     <div className="fluid-conatiner">
       <div className="container">
         <div className="col-lg-8 mx-auto">
-          <div class="card mt-5">
-            <div class={`card-header p-3 ${styles.head}`}>
+          <div className="card mt-5">
+            <div className={`card-header p-3 ${styles.head}`}>
               Community Details
             </div>
-            <div class="card-body">
+            <div className="card-body">
               <form>
-                <div class="form-group">
-                  <label for="name">Name</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="name"
-                    placeholder="Name..."
+                <div className="form-group">
+                  <Input name="Name" value={name} onChange={handleName} />
+                </div>
+                <div className="form-row">
+                  <div className="col-md-6">
+                    <Input
+                      name="Location"
+                      value={location}
+                      onChange={handleLocation}
+                    />
+                  </div>
+                  <div className="col-md-6">
+                    <Input
+                      name="Size"
+                      value={size}
+                      onChange={handleSize}
+                      type="number"
+                    />
+                  </div>
+                </div>
+                <div className="col-10 mx-auto mb-3" styles={styles.domain}>
+                  <CreatableSelect
+                    value={option.selectedOption}
+                    isMulti
+                    onChange={handleChange}
+                    options={defaultOptions}
+                    closeMenuOnSelect={false}
+                    placeholder="Domain"
                   />
                 </div>
-                <div class="form-row">
-                  <div class="form-group col-md-6">
-                    <label for="location">Location</label>
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="location"
-                      placeholder="Location..."
-                    />
-                  </div>
-                  <div class="form-group col-md-6">
-                    <label for="size">Team Size</label>
-                    <input
-                      type="number"
-                      class="form-control"
-                      id="size"
-                      placeholder="Team Size..."
-                    />
+                <div className="form-group">
+                  <div className="form-row">
+                    <div className="col-md-6">
+                      <Input
+                        name="Website"
+                        value={web}
+                        onChange={handleWeb}
+                        placeholder="Website"
+                      />
+                    </div>
+                    <div className="col-md-6">
+                      <Input name="Facebook" value={fb} onChange={handleFB} />
+                    </div>
+                    <div className="col-md-6">
+                      <Input
+                        name="Instagram"
+                        value={insta}
+                        onChange={handleInsta}
+                      />
+                    </div>
+                    <div className="col-md-6">
+                      <Input
+                        name="Twitter"
+                        value={twitter}
+                        onChange={handleTwitter}
+                      />
+                    </div>
+                    <div className="col-md-6">
+                      <Input name="Linked In" value={In} onChange={handleIN} />
+                    </div>
+                    <div className="col-md-6">
+                      <Input
+                        name="Medium"
+                        value={medium}
+                        onChange={handleMedium}
+                      />
+                    </div>
+                    <div className="col-md-6">
+                      <Input name="Github" value={git} onChange={handleGit} />
+                    </div>
+                    <div className="col-md-6">
+                      <Input name="YouTube" value={yt} onChange={handleYT} />
+                    </div>
                   </div>
                 </div>
-                <div class="form-group row">
-                  <label for="domain" class="col-sm-2 col-form-label">
-                    Domains
-                  </label>
-                  <div class="col-sm-10">
-                    <CreatableSelect
-                      value={option.selectedOption}
-                      isMulti
-                      onChange={handleChange}
-                      options={defaultOptions}
-                      closeMenuOnSelect={false}
-                    />
-                  </div>
+                <div className="text-center">
+                  <button
+                    type="submit"
+                    className={`col-lg-6 col-sm-6 col-md-6 btn btn-primary mx_auto ${styles.add}`}
+                  >
+                    Add Community
+                  </button>
                 </div>
-                <div class="form-row">
-                  {fields.map(item => {
-                    return (
-                      // <div class="form-group col-md-6">
-                      //   <label for="location">{item}</label>
-                      //   <input
-                      //     type="text"
-                      //     class="form-control"
-                      //     id="location"
-                      //     placeholder={`Enter ${item} link`}
-                      //   />
-                      // </div>
-                      <div className="col-md-6">
-                        <Input name={item} />
-                      </div>
-                    );
-                  })}
-                </div>
-                <button type="submit" class="btn btn-primary">
-                  Add
-                </button>
               </form>
             </div>
           </div>
--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
-const Input = ({ name, ...rest }) => {
+const Input = ({ name, onChange, value, ...rest }) => {
   return (
     <div className="form-group">
-      <label htmlFor={name}>{name}</label>
+      {/* <label htmlFor={name}>{name}</label> */}
       <input
         {...rest}
         name={name}
         className="form-control"
         id={name}
-        value={name}
+        value={value}
+        onChange={onChange}
+        placeholder={name}
       />
       {/* {errors && <div className="alert alert-danger mt-2">{errors}</div>} */}
     </div>
