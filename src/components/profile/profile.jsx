25b80fe6f270f46d650c0d94c1b919b6df800d1f
mayanksh99 <mayankshakya992@gmail.com>
2020-03-22 19:29:47 +0530

    ---fix:some changes

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../../contexts/userContext";
 import styles from "./profile.module.css";
+import { getdata } from "../../utils/routes";
+import axios from "axios";
 
 export default function Profile() {
+  let data = useContext(AuthContext);
+  const [profile, setProfile] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let response = await axios.get(`${getdata}?id=${data.user.user.id}`);
+        setProfile(response.data);
+      } catch (error) {
+        console.log(error.response);
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div className="container">
       <div className="col-lg-10 mx-auto">
@@ -22,44 +39,58 @@ export default function Profile() {
               />
             </div>
           </div>
-          <div className="text-center mt-4">
-            <p style={{ fontSize: "23px" }}>Community Details</p>
-          </div>
-          <div className="row mt-4">
-            <div className="col-lg-4 text-center">
-              <img
-                className="img-fluid"
-                src="./assets/images/team.png"
-                alt=""
-                width="60"
-              />
-              <p className="mt-4" style={{ fontSize: "20px" }}>
-                56
-              </p>
-            </div>
-            <div className="col-lg-4 text-center">
-              <img
-                className="img-fluid mt-1"
-                src="./assets/images/domain.png"
-                alt=""
-                width="60"
-              />
-              <p className="" style={{ fontSize: "20px", marginTop: "28px" }}>
-                Web, ML, Android, Cloud
-              </p>
-            </div>
-            <div className="col-lg-4 text-center">
-              <img
-                className="img-fluid mt-1"
-                src="./assets/images/location.png"
-                alt=""
-                width="60"
-              />
-              <p className="" style={{ fontSize: "20px", marginTop: "19px" }}>
-                Pune, India
-              </p>
-            </div>
-          </div>
+          {profile ? (
+            profile.data.isPublished === true ? (
+              <>
+                <div className="text-center mt-4">
+                  <p style={{ fontSize: "23px" }}>Community Details</p>
+                </div>
+                <div className="row mt-4">
+                  <div className="col-lg-4 text-center">
+                    <img
+                      className="img-fluid"
+                      src="./assets/images/team.png"
+                      alt=""
+                      width="60"
+                    />
+                    <p className="mt-4" style={{ fontSize: "20px" }}>
+                      56
+                    </p>
+                  </div>
+                  <div className="col-lg-4 text-center">
+                    <img
+                      className="img-fluid mt-1"
+                      src="./assets/images/domain.png"
+                      alt=""
+                      width="60"
+                    />
+                    <p
+                      className=""
+                      style={{ fontSize: "20px", marginTop: "28px" }}
+                    >
+                      Web, ML, Android, Cloud
+                    </p>
+                  </div>
+                  <div className="col-lg-4 text-center">
+                    <img
+                      className="img-fluid mt-1"
+                      src="./assets/images/location.png"
+                      alt=""
+                      width="60"
+                    />
+                    <p
+                      className=""
+                      style={{ fontSize: "20px", marginTop: "19px" }}
+                    >
+                      Pune, India
+                    </p>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <h1>Your profile is submitted for reveiw</h1>
+            )
+          ) : null}
         </div>
       </div>
     </div>
